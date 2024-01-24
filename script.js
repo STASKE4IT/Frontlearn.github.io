8d1c5808f6c5839b0c33041126cb16699dcd3505
STASKE <STASKE4IT@gmail.com>
2024-01-24 18:43:12 +0500

    changed

--- script.js
+++ script.js
@@ -8,23 +8,39 @@ let password2=document.querySelectorAll(".password")[1]
 // Кнопки
 let btn=document.querySelector(".btn")
 
-btn.addEventListener("click", comeon)
+btn.addEventListener("click", clickMe)
 
 //Регулярки
 let userNameReg = /^[A-Za-zА-Яа-я\- ]*[^0-9][A-Za-zА-Яа-я\- ]*$/;
 let userEmailReg= /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/
 let userPasswordReg= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/
 
-function comeon(event){
+function clickMe(event){
     event.preventDefault()
     validate(userNameReg, username)
     validate(userNameReg, lastname)
+    // validate(userEmailReg, email)
+    // validate(userPasswordReg, password1)
+    // validate(userPasswordReg, password2)
 
 }
 function validate(userReg,userValue){
     
     if(!userReg.test(username.value)){
-        userValue.classList.add("error")
+        alert ("Некорректное имя")
     }
+//     }else{
+//         if(!userReg.test(email.value)){
+//             userValue.classList.add("error")
+//         }else{
+//             if(!userReg.test(password1.value)){
+//                 userValue.classList.add("error")
+//             }else{
+//                 if(!userReg.test(password2.value)){
+//                     userValue.classList.add("error")
+//                 }
+//             }
+//         }
+//     }
 }
-
+// console.log(validate)
